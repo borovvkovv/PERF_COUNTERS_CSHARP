ae6536c204d02229aa767a71b4f683694422573c
Dmitry <borovvkovv@gmail.com>
2021-09-28 21:07:35 +0500

    1

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PERF_COUNTERS_CSHARP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
